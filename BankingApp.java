99c2dd9bea791e5ed21989a2e2382a07c39a3b1d
Fred Butler <90980495+thefreddieb@users.noreply.github.com>
2022-02-21 22:28:40 -0600

    Add files via upload

---
+++ BankingApp.java
@@ -0,0 +1,13 @@
+
+public class BankingApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Account fred = new Account("Fred Butler", "A00001");
+		fred.showMenu();
+		
+		
+	}
+
+}
